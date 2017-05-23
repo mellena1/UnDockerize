3ea0a1335d053aefb61919da8d0b479114e671ee
Andrew Mellen <andrew_mellen@icloud.com>
2017-05-23 17:53:00 -0400

    Condensed similar code
    Organized a bunch of stuff and tried to condense as much as possible

--- UnDockerize.py
+++ UnDockerize.py
@@ -1,11 +1,22 @@
 import argparse
 
+"""
+Docker Class
+-------------
+Holds the Docker file info and parses it all
+"""
 class Docker:
-    #Returns an array of all of the lines in the docker file
+    #Instantiates an array with all of the lines in the given docker file
     def __init__(self, file_name):
         #instance vars
         self.docker_file = []
         self.ansible_file = ['---']
+        self.cases = { #different cases for the docker file syntax
+                        'ADD'   : self.COPY,
+                        'COPY'  : self.COPY,
+                        'ENV'   : self.ENV,
+                        'RUN'   : self.RUN
+                     }
         #Read the file in and put the lines in the docker_file array
         with open(file_name, 'r') as f:
             for line in f:
@@ -14,13 +25,8 @@ class Docker:
     #Parses each line of docker to return the ansible file array
     def parse_docker(self):
         docker_file = self.docker_file
-        #different cases for the docker file syntax
-        cases = {
-                    'RUN'   : self.RUN
-                    'LABEL' : self.LABEL
-                    'ENV'   : self.ENV
-                    'ADD'   : self.ADD
-                }
+        ansible_file = self.ansible_file
+        cases = self.cases
 
         #Check each line for command, run cooresponding function
         for x in range(0,len(docker_file)):
@@ -29,35 +35,29 @@ class Docker:
                 command = line_split[0]
                 if command in cases:
                     cases[command](x)
+                    ansible_file.append('') #add new line after command
+        del self.ansible_file[-1] # remove the last \n
 
-    #Logic for a RUN docker command
+    """--------------------------COMMANDS---------------------------------"""
+    #Logic for a COPY command (Copies file to another location)
+    def COPY(self, x):
+        cmd = '  shell: cp '+self.condense_multiline_cmds(x)
+        put_together(x, name='ADD', cmd)
+
+    #Logic for a ENV command (Sets environment variables)
+    def ENV(self, x):
+        cmd = '  shell: export '+self.ENV_helper(self.condense_multiline_cmds(x))
+        put_together(x, name='ENV', cmd)
+
+    #Logic for a RUN command (Shell command)
     def RUN(self, x):
-        docker_file = self.docker_file
-        ansible_file = self.ansible_file
+        cmd='  shell: ' + self.condense_multiline_cmds(x)
+        put_together(x, name='RUN', cmd)
 
-        self.COMMENTS(x-1)
-        ansible_file.append('- name: RUN')
 
-        #Account for backslashes to continue RUN command across new lines
-        line = ''
-        while True:
-            line_split = docker_file[x].split()
-
-            if line_split[0] == 'RUN': #Remove RUN from split
-                line_split = line_split[1:]
-
-            if line_split[len(line_split)-1] == '\\': #Has backslash
-                line += ' '.join(line_split[:len(line_split)-1]) + ' '
-                x += 1
-            else: #End of a statement
-                line += ' '.join(line_split)
-                break
-
-        ansible_file.append('  shell: ' + line)
-        ansible_file.append('') #add new line after command
-
-    #takes all comments from y up and appends them
-    def COMMENTS(self, y):
+    """------------------COMMAND HELPER FUNCTIONS-------------------------"""
+    #Takes all comments from y up and appends them (Usually pass x-1)
+    def comments(self, y):
         docker_file = self.docker_file
 
         #Include comments above the RUN command
@@ -74,26 +74,55 @@ class Docker:
         if comments != '':
             ansible_file.append(comments)
 
+    #Account for backslashes to condense multiline command into one line
+    def condense_multiline_cmds(self, x):
+        docker_file = self.docker_file
 
-    #def LABEL(self, x):
+        line = ''
+        while True:
+            line_split = docker_file[x].split()
+            if line_split[0] in self.cases: #Remove cases from split
+                line_split = line_split[1:]
+            if line_split[len(line_split)-1] == '\\': #Has backslash
+                line += ' '.join(line_split[:len(line_split)-1]) + ' '
+                x += 1
+            else: #End of a statement
+                line += ' '.join(line_split)
+                break
+        return line
 
-    #def ENV(self, x):
-    #    docker_file = self.docker_file
-    #    ansible_file = self.ansible_file
-    #    if x > 0 and docker_file[x - 1][0] == '#':
-    #        ansible_file.append('- name: ' + docker_file[x - 1][1:])
-    #        ansible_file.append('  shell: export ' + docker_file[x - 1][1] + ' ' + docker_file[x][1])
+    #ENV allows for either ENV VAR=val or ENV VAR val
+    #Change format to VAR=val for ansible
+    def ENV_helper(self, line):
+        line = line.split()
+        output = []
+        x = 0
+        while x < len(line):
+            if '=' not in line[x]:
+                output.append(line[x] + '=' + line[x+1])
+                x += 2
+            else:
+                output.append(line[x])
+                x += 1
+        return ' '.join(output)
 
-    def ADD(self, x):
-        ocker_file = self.docker_file
+    #The common stuff of every command
+    #Adds the comments above, then the name and
+    #   command lines to the ansible_file array
+    def put_together(self, x, name, cmd):
+        docker_file = self.docker_file
         ansible_file = self.ansible_file
-        if x > 0 and docker_file[x-1][0] == '#':
-            ansible_file.append('- name: ' + docker_file[x-1][1:])
-            ansible_file.append('cp ' + ' '.join(parsed[1:]))
-        else:
-            ansible_file.append('- name: Add command')
-            ansible_file.append('cp ' + ' '.join(parsed[1:]))
 
+        self.comments(x-1)
+        ansible_file.append('- name: ' + name)
+        ansible_file.append(cmd)
+
+
+"""
+Ansible Class
+------------------
+Holds the ansible info and writes the array to the yml file
+"""
 class Ansible:
     def __init__(self, ansible_array):
         self.ansible = ansible_array
@@ -108,7 +137,7 @@ class Ansible:
                 f.write(line + '\n')
 
 
-#Main
+#Main function
 if __name__ == "__main__":
     #command-line argument stuff
     #-i for input file
@@ -118,7 +147,10 @@ if __name__ == "__main__":
     argparser.add_argument('-o', nargs=1, default=['unDockerized'], type=str, metavar='output_file', help='The output (Ansible) file name; Default: UnDockerized')
     args = vars(argparser.parse_args())
 
+    #Parse Docker
     docker_file = Docker(args['i'][0])
     docker_file.parse_docker()
+
+    #Write to Ansible
     ansible_file = Ansible(docker_file.ansible_file)
     ansible_file.write_to_file(args['o'][0])
