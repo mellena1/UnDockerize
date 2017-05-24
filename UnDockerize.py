478d44b2dbd7edbe0b2df0b97e5134ff8e385edd
Andrew Mellen <andrew_mellen@icloud.com>
2017-05-24 14:49:51 -0400

    Fixed typo

--- UnDockerize.py
+++ UnDockerize.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     #-o for output file
     argparser = argparse.ArgumentParser(description='Convert a Dockerfile to Ansible code')
     argparser.add_argument('-i', nargs=1, default=['Dockerfile'], type=str, metavar='input_file', help='The input (Dockerfile) file name; Default: Dockerfile')
-    argparser.add_argument('-o', nargs=1, default=['unDockerized'], type=str, metavar='output_file', help='The output (Ansible) file name; Default: UnDockerized')
+    argparser.add_argument('-o', nargs=1, default=['UnDockerized'], type=str, metavar='output_file', help='The output (Ansible) file name; Default: UnDockerized')
     args = vars(argparser.parse_args())
 
     input_file = args['i'][0]
